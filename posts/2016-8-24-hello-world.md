c0f90456b9b3338d1869a55b84be5f4da4bce205
zhongdefu173 <zhongdefu173@lu.com>
2016-08-24 16:40:10 +0800

    init my blog

---
+++ posts/2016-8-24-hello-world.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: First post
+
+---
+# hello world!
+
+{{ page.date | date_to_string}}
--- srv/main/java/com/karl/test/HelloWord.java
+++
@@ -1,8 +0,0 @@
-package com.karl.test;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello word!");
-    }
-	
-}
