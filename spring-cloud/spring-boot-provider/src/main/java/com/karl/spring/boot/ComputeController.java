8b366f1d0617534dce53d261bcd63a2ed1e967eb
karl zhong <zhonghan08@aliyun.com>
2017-04-12 07:32:02 +0800

    spring-cloud

---
+++ spring-cloud/spring-boot-provider/src/main/java/com/karl/spring/boot/ComputeController.java
@@ -0,0 +1,28 @@
+package com.karl.spring.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by karl on 4/11/17.
+ */
+@RestController
+public class ComputeController {
+    private final Logger logger = Logger.getLogger("ComputeController");
+    @Autowired
+    private DiscoveryClient client;
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+}
---
+++ spring-cloud/spring-boot-reg/src/test/java/com/karl/spring/boot/data/jpa/repository/UserRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.karl.spring.boot.data.jpa.repository;
+
+import com.karl.spring.boot.data.jpa.User;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by karl on 4/8/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest()
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    // 打印出class com.sun.proxy.$Proxy66表示spring注入通过jdk动态代理获取接口的子类
+    @Test
+    public void proxy() throws Exception {
+        System.out.println(userRepository.getClass());
+    }
+
+    @Test
+    public void save() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            User user = new User("jege" + i, 25 + i);
+            userRepository.save(user);
+        }
+    }
+
+    @Test
+    public void all() throws Exception {
+        save();
+        assertThat(userRepository.findAll()).hasSize(10);
+    }
+
+    @Test
+    public void findByName() throws Exception {
+        save();
+        assertThat(userRepository.findByNameLike("jege%")).hasSize(10);
+    }
+
+    @After
+    public void destroy() throws Exception {
+        userRepository.deleteAll();
+    }
+
+}
